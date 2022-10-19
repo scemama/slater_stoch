4d6c4005f191085dce463686599592e6e7020b55
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2022-10-19 13:39:08 +0200

    1-e with error OK

--- clean_Vee.py
+++ clean_Vee.py
@@ -7,7 +7,10 @@ def read_one(filename):
     with open(filename,'r') as f:
         l = []
         for line in f:
+           try:
             i, j, x = line.replace('D','E').split()
+           except:
+            i, j, x, _ = line.replace('D','E').split()
             l.append( (int(i)-1, int(j)-1, float(x)) )
 
     ao_num = l[-1][0]+1
