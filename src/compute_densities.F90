d3357675e46662278012409d46691d1ce256903c
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2022-10-24 15:52:01 +0200

    Optimizations

--- src/compute_densities.F90
+++ src/compute_densities.F90
@@ -52,57 +52,44 @@ subroutine compute_densities(i,k,ut1,ut2,rho,rho_G,poly)
     poly (kw,ik,1)=poly_ik
     rho_G(kw,ik,1)=poly_ik*u_gauss(i,r_i)*u_gauss(k,r_k)
 
-    rho  (kw,ik,2)= phi_orb (i,ut2(1,kw,ik)) * phi_orb (k,ut2(1,kw,ik))
-    rho_G(kw,ik,2)= phi_gaus(i,ut2(1,kw,ik)) * phi_gaus(k,ut2(1,kw,ik))
+
+    dx=ut2(1,kw,ik)-center(1,i)
+    dy=ut2(2,kw,ik)-center(2,i)
+    dz=ut2(3,kw,ik)-center(3,i)
+
+    poly_i=1.d0
+    do kk=1,npower(1,i)
+      poly_i = poly_i * dx
+    enddo
+    do kk=1,npower(2,i)
+      poly_i = poly_i * dy
+    enddo
+    do kk=1,npower(3,i)
+      poly_i = poly_i * dz
+    enddo
+    r_i=dsqrt(dx*dx+dy*dy+dz*dz)
+
+    dx=ut2(1,kw,ik)-center(1,k)
+    dy=ut2(2,kw,ik)-center(2,k)
+    dz=ut2(3,kw,ik)-center(3,k)
+
+    poly_k=1.d0
+    do kk=1,npower(1,k)
+      poly_k = poly_k * dx
+    enddo
+    do kk=1,npower(2,k)
+      poly_k = poly_k * dy
+    enddo
+    do kk=1,npower(3,k)
+      poly_k = poly_k * dz
+    enddo
+    r_k=dsqrt(dx*dx+dy*dy+dz*dz)
+
+    poly_ik = poly_i*poly_k
+    rho  (kw,ik,2)=poly_ik*u_orb(i,r_i)*u_orb(k,r_k)
+    poly (kw,ik,2)=poly_ik
+    rho_G(kw,ik,2)=poly_ik*u_gauss(i,r_i)*u_gauss(k,r_k)
+
   enddo
-
-  contains
-
-
-  double precision function phi_orb(i_orb,x)
-    use common_data
-    integer, intent(in)            :: i_orb
-    double precision, intent(in)   :: x(3)
-    double precision               :: dx, dy, dz, r
-    integer                        :: i
-    double precision, external     :: u_orb
-    dx=x(1)-center(1,i_orb)
-    dy=x(2)-center(2,i_orb)
-    dz=x(3)-center(3,i_orb)
-    r=dsqrt(dx*dx+dy*dy+dz*dz)
-    phi_orb=u_orb(i_orb,r)
-    do i=1,npower(1,i_orb)
-      phi_orb  = phi_orb *dx
-    end do
-    do i=1,npower(2,i_orb)
-      phi_orb  = phi_orb *dy
-    end do
-    do i=1,npower(3,i_orb)
-      phi_orb  = phi_orb *dz
-    end do
-  end
-
-  double precision function phi_gaus(i_orb,x)
-    use common_data
-    integer, intent(in)            :: i_orb
-    double precision, intent(in)   :: x(3)
-    double precision               :: dx, dy, dz, r
-    integer                        :: i
-    double precision, external     :: u_gauss
-    dx=x(1)-center(1,i_orb)
-    dy=x(2)-center(2,i_orb)
-    dz=x(3)-center(3,i_orb)
-    r=dsqrt(dx*dx+dy*dy+dz*dz)
-    phi_gaus=u_gauss(i_orb,r)
-    do i=1,npower(1,i_orb)
-      phi_gaus = phi_gaus*dx
-    end do
-    do i=1,npower(2,i_orb)
-      phi_gaus = phi_gaus*dy
-    end do
-    do i=1,npower(3,i_orb)
-      phi_gaus = phi_gaus*dz
-    end do
-  end
 
 end
--- src/u_orb.F90
+++ src/u_orb.F90
@@ -6,7 +6,32 @@ double precision function u_orb(i_orb,r)
   integer :: mm
   double precision :: r2
   u_orb=0.d0
-  do mm=1,n_contract(i_orb)
-    u_orb=u_orb+c_contract(mm,i_orb)*r**n_sto(i_orb)*dexp(-g_contract(mm,i_orb)*r)
-  enddo
+  select case (n_sto(i_orb))
+    case (0)
+      do mm=1,n_contract(i_orb)
+        u_orb=u_orb+c_contract(mm,i_orb)*dexp(-g_contract(mm,i_orb)*r)
+      enddo
+    case (1)
+      do mm=1,n_contract(i_orb)
+        u_orb=u_orb+c_contract(mm,i_orb)*r*dexp(-g_contract(mm,i_orb)*r)
+      enddo
+    case (2)
+      r2 = r*r
+      do mm=1,n_contract(i_orb)
+        u_orb=u_orb+c_contract(mm,i_orb)*r2*dexp(-g_contract(mm,i_orb)*r)
+      enddo
+    case (3)
+      r2 = r*r
+      do mm=1,n_contract(i_orb)
+        u_orb=u_orb+c_contract(mm,i_orb)*r2*r*dexp(-g_contract(mm,i_orb)*r)
+      enddo
+    case (4)
+      do mm=1,n_contract(i_orb)
+        u_orb=u_orb+c_contract(mm,i_orb)*r2*r2*dexp(-g_contract(mm,i_orb)*r)
+      enddo
+    case default
+      do mm=1,n_contract(i_orb)
+        u_orb=u_orb+c_contract(mm,i_orb)*r**n_sto(i_orb)*dexp(-g_contract(mm,i_orb)*r)
+      enddo
+  end select
 end
