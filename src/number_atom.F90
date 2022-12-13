634a69f5844883c55a1c98751c7fd96afb1388fb
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2022-12-13 15:21:03 +0100

    Gfortran

--- src/number_atom.F90
+++ src/number_atom.F90
@@ -1,10 +1,10 @@
 integer function number_atom(ATOM)
   implicit none
   character(80), intent(in)      :: ATOM
-  character(2), parameter        :: a(18) = (/                       &
-      'H' ,                                   'He',                  &
-      'Li', 'Be', 'B' , 'C' , 'N', 'O', 'F' , 'Ne',                  &
-      'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar'  /)
+  character(2), parameter        :: a(18) = (/                         &
+      'H ' ,                                    'He',                  &
+      'Li', 'Be', 'B ', 'C ', 'N ', 'O ', 'F ', 'Ne',                  &
+      'Na', 'Mg', 'Al', 'Si', 'P ', 'S ', 'Cl', 'Ar'  /)
   integer :: i
   do i=1,size(a)
     if (ATOM == a(i)) then
